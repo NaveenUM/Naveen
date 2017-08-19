d448161cc8cdbd35c5c0a0372d8bdc98943dc929
NaveenUM <30368598+NaveenUM@users.noreply.github.com>
2017-08-19 16:49:40 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    double n1, n2, n3;
+    printf("Enter three different numbers: ");
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+    if
+        printf("%.2f is the largest number.", n1);
+    if
+        printf("%.2f is the largest number.", n2);
+    if
+        printf("%.2f is the largest number.", n3);
+
+    return 0;
+}
