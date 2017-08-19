87cfa10a6344c72a0c31a2b79092f9eee0b47ce4
NaveenUM <30368598+NaveenUM@users.noreply.github.com>
2017-08-19 16:55:05 +0530

    Update number.c

--- number.c
+++ number.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+ 
 int main()
-{
-    double n1, n2, n3;
-    printf("Enter three different numbers: ");
-    scanf("%lf %lf %lf", &n1, &n2, &n3);
-    if
-        printf("%.2f is the largest number.", n1);
-    if
-        printf("%.2f is the largest number.", n2);
-    if
-        printf("%.2f is the largest number.", n3);
-
+{ 
+    int a,b,c;
+    int largest;
+     printf("Enter three numbers (separated by space):");
+    scanf("%d%d%d",&a,&b,&c);
+    if       
+        largest=a;
+    else if       
+        largest=b;
+    else
+        largest=c;
+ 
+    printf("Largest number is = %d",largest);
+ 
     return 0;
 }
