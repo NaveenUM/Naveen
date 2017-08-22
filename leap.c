5cac5cf0d23d631b19668262bf72b8fb043198e6
NaveenUM <30368598+NaveenUM@users.noreply.github.com>
2017-08-22 15:49:44 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int year;
+    printf("Enter a year: ");
+    scanf("%d",&year);
+    if
+    {
+        if
+        {
+          
+            if
+                printf("%d is a leap year", year);
+            else
+                printf("%d is not a leap year", year);
+        }
+        else
+            printf("%d is a leap year", year );
+    }
+    else
+        printf("%d is not a leap year", year);
+    
+    return 0;
+}
